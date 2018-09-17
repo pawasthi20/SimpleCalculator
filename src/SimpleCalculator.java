660b8321efe981114fd1acaa08a8614bb2e2b23b
pawasthi20 <43074073+pawasthi20@users.noreply.github.com>
2018-09-17 13:38:59 -0400

    Add files via upload

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,11 @@
+
+public class SimpleCalculator
+{
+
+	public int add(int num1,int num2)
+	{
+		int ans = num1 + num2;
+		return ans;
+	}
+	
+}
---
+++ src/SimpleCalculatorMain.java
@@ -0,0 +1,12 @@
+
+public class SimpleCalculatorMain {
+
+	public static void main(String[] args) 
+	{
+		//This is how we creat an object
+		//new ->java keyword which creates an object
+		SimpleCalculator simpleCalculator = new SimpleCalculator();
+		System.out.print(simpleCalculator.add(2, 6));
+	}
+
+}
